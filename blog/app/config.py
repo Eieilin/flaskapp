2691773cc473cce7730602291c925771c17ca090
Eieilin <Eieilin2123@gmail.com>
2018-08-22 11:58:35 +0630

    Still have error

--- blog/app/config.py
+++ blog/app/config.py
@@ -1,2 +1,5 @@
+import os
 class Configuration(object):
+    APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG = True
+    SQLALCHEMY_DATABASSE_URI = 'sqlite:///%s/blog.db' % APPLICATION_DIR
