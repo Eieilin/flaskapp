bc090b33010f2e31604c7de440775f0a7ebe78bb
Eieilin <Eieilin2123@gmail.com>
2018-08-24 20:51:00 +0630

    I can slove error

--- blog/app/config.py
+++ blog/app/config.py
@@ -2,4 +2,5 @@ import os
 class Configuration(object):
     APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG = True
-    SQLALCHEMY_DATABASSE_URI = 'sqlite:///%s/blog.db' % APPLICATION_DIR
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/blog.db' % APPLICATION_DIR
+    SQLALCHEMY_TRACK_MODIFICATIONS = 'Entry'
